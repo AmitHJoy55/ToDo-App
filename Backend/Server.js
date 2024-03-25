06a1a42f9c772658eeed04ed246f12637a0d77ac
Amit Hassan Joy <sarkeramit88@gmail.com>
2024-03-26 01:50:35 +0600

    Backend Added with all the routes.
    Complete backend added & for frontend we actually install all the dependencies of reactjs .

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
-require('dotenv').config()
+const cors = require("cors")
 
-const app = express()
+const routes = require('./routes/ToDoRoutes')
+
+require('dotenv').config() 
+
+
+
+const app = express() ;
 const PORT = process.env.port || 5000
+
+app.use(express.json())
+app.use(cors())
 
 mongoose
     .connect(process.env.MONGODB_URL)
     .then(()=> console.log(`Connected To MongoDB...`))
     .catch((err)=> console.log(err))
+   
+app.use(routes);
 
 
-
-app.listen(PORT , ()=> console.log(`Listening on : ${PORT} `) )
+app.listen(PORT , ()=> console.log(`Listening on : ${PORT} `) );
---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    <div className="container">
+      <h1>ToDo APP</h1>
+      
+      <div className="top">
+        <input type="text" placeholder="Add Todos.."></input>
+        <div className="add">Add</div>
+
+      </div>
+
+    </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
